53fd6a3423250b067b5a2f337679258b2b23ded4
Anjali <anjaliarun0609@gmail.com>
2026-04-16 03:09:16 +0530

    closing directory, serializing the tree and writing tree obj

--- tree.c
+++ tree.c
@@ -171,12 +171,12 @@ int tree_from_index(ObjectID *id_out) {
     
     closedir(dir);
 
-    /* Serialize tree */
+
     void *data;
     size_t len;
     if (tree_serialize(&tree, &data, &len) < 0) return -1;
 
-    /* Write tree object */
+
     if (object_write(OBJ_TREE, data, len, id_out) < 0) {
         free(data);
         return -1;
