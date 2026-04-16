8021e17857e58f258e3bfb452371da769d710bac
Anjali <anjaliarun0609@gmail.com>
2026-04-16 10:38:33 +0530

    checking for parent and then updating commit structure

--- commit.c
+++ commit.c
@@ -201,6 +201,27 @@ int commit_create(const char *message, ObjectID *commit_id_out) {
     ObjectID parent;
     int has_parent = (head_read(&parent) == 0);
 
+    Commit c = {0};
+
+    c.tree = tree;
+    if (has_parent) {
+        c.parent = parent;
+        c.has_parent = 1;
+    } else {
+        c.has_parent = 0;
+    }
+
+    const char *author_str = pes_author();
+    if (!author_str) author_str = "Unknown";
+
+    char author_line[256];
+    snprintf(author_line, sizeof(author_line), "%s <%ld@localhost>",
+             author_str, (long)time(NULL));
+    snprintf(c.author, sizeof(c.author), "%s", author_line);
+    c.timestamp = (uint64_t)time(NULL);
+
+    snprintf(c.message, sizeof(c.message), "%s", message);
+
     
 
     return 0;
